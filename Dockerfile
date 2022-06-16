32c1a59f0f7a5904321074b09eca5a7bbc724c13
limeng <limeng32@live.cn>
2022-06-16 15:06:49 +0800

    Use iotdb-ui-0.12.5.jar and dist-0.12.5/ ;

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,10 @@
-FROM nginx
+FROM nginx:1.21.0
 
 COPY start.sh /root/start.sh
 
 COPY zulu-8/ /usr/zulu-8/
 
-COPY iotdb-ui-0.12.4.jar /root/iotdb-ui.jar
+COPY iotdb-ui-0.12.5.jar /root/iotdb-ui.jar
 
 COPY iotdbui.db /root/iotdbui.db
 
@@ -12,7 +12,7 @@ COPY application.yml /root/application.yml
 
 COPY nginx.conf /etc/nginx/nginx.conf
 
-COPY dist-0.12.4/ /usr/share/nginx/html
+COPY dist-0.12.5/ /usr/share/nginx/html
 
 RUN chmod -R +x /usr/zulu-8/
 
--- start.sh
+++ start.sh
@@ -2,17 +2,46 @@
 export JAVA_HOME=/usr/zulu-8
 export PATH=$PATH:$JAVA_HOME/bin:$JAVA_HOME/jre/bin
 
-sed -i "s/_iotdbui_frontend_/$iotdbui_frontend/" /root/application.yml
-sed -i "s/_iotdbui_email_port_/$iotdbui_email_port/" /root/application.yml
-sed -i "s/_iotdbui_email_host_/$iotdbui_email_host/" /root/application.yml
-sed -i "s/_iotdbui_email_username_/$iotdbui_email_username/" /root/application.yml
-sed -i "s/_iotdbui_email_password_/$iotdbui_email_password/" /root/application.yml
-
 if [ -z $iotdbui_db_name ]
 then
    sed -i "s/_iotdbui_db_name_/iotdbui\.db/" /root/application.yml
 else
    sed -i "s/_iotdbui_db_name_/$iotdbui_db_name/" /root/application.yml
+fi
+
+if [ -z $iotdbui_frontend ]
+then
+   sed -i "s/_iotdbui_frontend_/127\.0\.0\.1:80/" /root/application.yml
+else
+   sed -i "s/_iotdbui_frontend_/$iotdbui_frontend/" /root/application.yml
+fi
+
+if [ -z $iotdbui_email_port ]
+then
+   sed -i "s/_iotdbui_email_port_/465/" /root/application.yml
+else
+   sed -i "s/_iotdbui_email_port_/$iotdbui_email_port/" /root/application.yml
+fi
+
+if [ -z $iotdbui_email_host ]
+then
+   sed -i "s/_iotdbui_email_host_//" /root/application.yml
+else
+   sed -i "s/_iotdbui_email_host_/$iotdbui_email_host/" /root/application.yml
+fi
+
+if [ -z $iotdbui_email_username ]
+then
+   sed -i "s/_iotdbui_email_username_//" /root/application.yml
+else
+   sed -i "s/_iotdbui_email_username_/$iotdbui_email_username/" /root/application.yml
+fi
+
+if [ -z $iotdbui_email_password ]
+then
+   sed -i "s/_iotdbui_email_password_//" /root/application.yml
+else
+   sed -i "s/_iotdbui_email_password_/$iotdbui_email_password/" /root/application.yml
 fi
 
 /usr/sbin/nginx -c /etc/nginx/nginx.conf -g 'daemon on;'
